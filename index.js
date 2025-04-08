dbdbc5dd25bf7c112a996bf536e3e198da5c7ad2
Guillaume Bourlart <62341350+GuillaumeBourlart@users.noreply.github.com>
2025-04-08 20:15:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ function randomItemRadius() {
 // Pour la hitbox du joueur, on définit la taille de la tête et des segments
 function getHeadRadius(player) {
   // La tête grossit en fonction du nombre d'items mangés (donc de segments dans la queue)
-  return BASE_SIZE / 2 + player.itemEatenCount * 0.1;
+  return BASE_SIZE / 2 + player.itemEatenCount * 0.1 * 1.2;
+
 }
 
 function getSegmentRadius(player) {
