07506566b8b05986658a3a24ae228cf45af16e45
Guillaume Bourlart <62341350+GuillaumeBourlart@users.noreply.github.com>
2025-04-11 12:58:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -303,7 +303,6 @@ function getVisiblePlayersForPlayer(player, allPlayers) {
 
 // --------------------------------------------------------------
 
-const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*" } });
 app.use(cors({ origin: "*" }));
 
