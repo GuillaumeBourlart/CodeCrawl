05cf959cbd5299e53186127c3b0a02520fb05ad4
Guillaume Bourlart <62341350+GuillaumeBourlart@users.noreply.github.com>
2025-04-30 05:31:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -685,7 +685,7 @@ if (roomsData[roomId] && Object.keys(roomsData[roomId].players).length === 0) {
 
 // Intervalle de mise à jour groupée du leaderboard
 setInterval(async () => {
-  console.time("leaderboardUpdate");
+  //console.time("leaderboardUpdate");
   const updates = Object.entries(scoreUpdates).map(([id, data]) => ({ id, ...data }));
   if (updates.length > 0) {
     const { error } = await supabase.from("global_leaderboard").upsert(updates);
@@ -694,13 +694,13 @@ setInterval(async () => {
     }
     Object.keys(scoreUpdates).forEach(key => delete scoreUpdates[key]);
   }
-  console.timeEnd("leaderboardUpdate");
+  //console.timeEnd("leaderboardUpdate");
 }, 10000);  // Toutes les 10 000ms (10 secondes)
 
 // --------------------------------------------------------------
 // Boucle principale de mise à jour du jeu : collisions, trajectoire & queue
 setInterval(() => {
-  console.time("gameLoop");
+ //console.time("gameLoop");
   Object.keys(roomsData).forEach(roomId => {
     const room = roomsData[roomId];
     const playerIds = Object.keys(room.players);
@@ -919,7 +919,7 @@ setInterval(() => {
       
     }
   });
-   console.timeEnd("gameLoop");
+   //console.timeEnd("gameLoop");
 }, 16);
 
 // Routes HTTP de base
